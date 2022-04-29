b4157b25eed140d5e953701b3a53189e785c15af
ichi944 <ichi944g@gmai..com>
2022-04-29 20:58:45 +0900

    Fix: remove unnecessary modules.

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,5 +1,4 @@
 pub mod auth_handler;
 pub mod user_handler;
-pub mod message_handler;
 pub mod home_handler;
 pub mod app_handler;
--- src/models/mod.rs
+++ src/models/mod.rs
@@ -1,5 +1,4 @@
 pub mod user;
 pub mod course;
 pub mod result;
-pub mod message;
 pub mod session;
