76828ec0627869cccca143478019abadd92eb78f
ichi944 <ichi944g@gmai..com>
2022-05-05 16:43:40 +0900

    Feature: sample fetch from graphql api

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,40 +3,35 @@ import logo from './logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const handleClick = () => {
+    console.log('ok')
+    fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        query: `
+          query {
+            users {
+              id
+              email
+              results {
+                courseId
+              }
+            }
+          }
+        `
+      })
+    })
+    .then(res => res.json())
+    .then(json => console.log('result', json))
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Hello, Sumire!
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+        <button onClick={handleClick}>Fetch!</button>
       </header>
     </div>
   )
