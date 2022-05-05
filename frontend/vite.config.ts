bbb14da156d086c39da50e562235d8caa67f96e2
ichi944 <ichi944g@gmai..com>
2022-05-05 16:35:30 +0900

    Fix: fix url for auth

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,14 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '../public',
+  build: {
+    outDir: '../public',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'js/main.js',
+      }
+    }
+  },
   server: {
     proxy: {
       '^/$': 'http://localhost:3001',
-      '/login': 'http://localhost:3001',
-      '/logout': 'http://localhost:3001',
       '/auth': 'http://localhost:3001',
-      '/graphql': 'http://localhost:3001',
       '/app': 'http://localhost:3001',
+      '/graphql': 'http://localhost:3001',
     }
   }
 })
