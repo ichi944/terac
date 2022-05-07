333a997852a13deecc11054e83658ac7f794d0f7
ichi944 <ichi944g@gmai..com>
2022-05-07 16:18:54 +0900

    Feature: add recoil and manage drawer state

--- frontend/src/components/Home.tsx
+++ frontend/src/components/Home.tsx
@@ -1,7 +1,30 @@
-import React from "react"
+import React from "react";
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
+} from "recoil";
+import { DrawerStateAtom } from "../atoms/DrawerStateAtom";
+import { AppDrawer } from "./AppDrawer";
 
 export const Home = () => {
-  return <div>
-    Home Screen
-  </div>
-}
+  const drawerIsOpen = useRecoilValue(DrawerStateAtom);
+  const setDrawerState = useSetRecoilState(DrawerStateAtom);
+  const handleToggleDrawer = () => {
+    setDrawerState((state) => ({ ...state, isOpen: !state.isOpen }));
+  };
+  const onClose = () => {
+    setDrawerState((state) => ({ ...state, isOpen: false }));
+  };
+  return (
+    <>
+      <AppDrawer isOpen={drawerIsOpen.isOpen} onClose={onClose} />
+      Home Screen
+      <button onClick={handleToggleDrawer}>Toggle!</button>
+
+    </>
+  );
+};
